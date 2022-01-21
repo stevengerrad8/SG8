b8e1c471b1dfa441254bb995d291ddac94835519
leeyo <leeyo@host.docker.internal>
2022-01-22 07:58:18 +0900

    # WARNING: head commit changed in the meantime
    Change-Id: I54c714b4cba077c763049e48d2989c091c00f6f5

---
+++ src/main/java/com/pro/main/VueProApplication.java
@@ -0,0 +1,13 @@
+package com.pro.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueProApplication.class, args);
+	}
+
+}
